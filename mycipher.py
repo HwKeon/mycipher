0ee696426b4d552801f679d4d99856cef57d34b7
HwKeon <131828035+HwKeon@users.noreply.github.com>
2024-04-28 00:22:29 -0400

    Add files via upload
    First function definition

--- mycipher.py
+++ mycipher.py
@@ -1,1 +1,12 @@
 import sys
+def caesar_cipher(text, shift):
+    encrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            # Determines if the character is uppercase
+            if char.isupper():
+                encrypted_char = chr((ord(char) - 65 + shift) % 26 + 65)
+            else:
+                encrypted_char = chr((ord(char) - 97 + shift) % 26 + 97)
+            encrypted_text += encrypted_char
+    return encrypted_text
